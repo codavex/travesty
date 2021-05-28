dbc7781049af1cd4b9071c6049d5039d6595dcc6
David Cox <dave@codavex.co.uk>
2021-05-28 22:37:29 +0100

    black'd

--- create_travesty.py
+++ create_travesty.py
@@ -4,7 +4,7 @@ import argparse
 import random
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "textLength", help="length of text to generate", type=int )
+parser.add_argument("textLength", help="length of text to generate", type=int)
 parser.add_argument("file", help="file to use as source")
 args = parser.parse_args()
 
@@ -14,14 +14,13 @@ travesty_file.close()
 
 travesty_dict = eval(travesty_text)
 
-key=random.choice(list(travesty_dict.keys()))
+key = random.choice(list(travesty_dict.keys()))
 
-print(key, end='')
+print(key, end="")
 
-i=0;
+i = 0
 while i < args.textLength:
     char = random.choice(list(travesty_dict[key].keys()))
-    print(char, end='')
+    print(char, end="")
     key = key[1:] + char
     i += 1
-
