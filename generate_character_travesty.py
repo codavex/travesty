82329d332ea837cb2b9157421ba27964cb4adab1
David Cox <dave@codavex.co.uk>
2021-05-30 22:46:52 +0100

    create better random characters

--- generate_character_travesty.py
+++ generate_character_travesty.py
@@ -23,10 +23,17 @@ print(key, end="")
 
 # pick character at random, print, then update key
 for i in range(args.textLength):
-    char = random.choice(list(statisticsDict[key].keys()))
-    print(char, end="")
-    key = key[1:] + char
+    probability = random.random()
+    nextChar = ""
+    for char in statisticsDict[key]:
+        nextChar = char
+        if float(statisticsDict[key][char]) < probability:
+            break
+    print(nextChar, end="")
+    key = key[1:] + nextChar
     if key not in statisticsDict:
         # on off-chance the key doesn't exist, select a new one
         # (eg last key size string of source text is unique)
         key = random.choice(list(statisticsDict.keys()))
+
+print("")
